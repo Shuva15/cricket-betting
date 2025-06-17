da4a213e09a20c82ec4d25bf451328644c2dc8af
shuva <mahantashuva@gmail.com>
2025-06-17 19:15:09 +0530

    wrote tests for previous instructions, added more instructions

--- programs/cricket-betting/src/lib.rs
+++ programs/cricket-betting/src/lib.rs
@@ -2,15 +2,121 @@ use anchor_lang::prelude::*;
 
 declare_id!("F9UsdGmoyHAbp5oEqbGQNyuczUzo6fhbdMCHT9ASHmbU");
 
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The betting period is closed")]
+    BettingClosed,
+}
+
 #[program]
 pub mod cricket_betting {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_game(ctx: Context<CreateGame>, game_id: u64) -> Result<()> {
+        let game_account = &mut ctx.accounts.game_account;
+
+        game_account.game_id = game_id;
+        game_account.is_betting_open = true;
+        game_account.total_bettors = 0;
+        game_account.is_game_over = false;
+        game_account.result_runs = None;
+        game_account.result_wickets = None;
+        game_account.prize_distributed = false;
+        game_account.bump = ctx.bumps.game_account;
+
+        msg!("Game account created successfully");
         Ok(())
+    }
+
+    pub fn place_bet(ctx: Context<PlaceBet>, runs: u16, wickets: u8) -> Result<()> {
+        let bettor_account = &mut ctx.accounts.bettor_account;
+        let game_account = &mut ctx.accounts.game_account;
+
+        require!(game_account.is_betting_open, ErrorCode::BettingClosed);
+
+        bettor_account.runs = runs;
+        bettor_account.wickets = wickets;
+        bettor_account.claimed = false;
+        bettor_account.bump = ctx.bumps.bettor_account;
+
+        game_account.total_bettors += 1;
+
+        msg!("Bet placed");
+        Ok(())
+    }
+
+    // call this instruction from backend with the help of a oracle or api when 6th over is finished (don't know if this it how it's done)
+    pub fn update_betting_stop(ctx: Context<UpdateGameAccount>) -> Result<()> {
+        let game_account = &mut ctx.accounts.game_account;
+
+        game_account.is_betting_open = false;
+
+        msg!("Betting closed");
+        Ok(())
+    }
+
+    // call this instruction from backend with the match result
+    pub fn update_game_result(ctx: Context<UpdateGameAccount>, runs: u16, wickets: u8) -> Result<()> {
+        let game_account = &mut ctx. accounts.game_account;
+
+        game_account.is_game_over = true;
+        game_account.result_runs = Some(runs);
+        game_account.result_wickets = Some(wickets);
+
+        msg!("Result updated");
+        Ok(())
+    }
+
+    pub fn distribute_prize(ctx: Context<UpdateGameAccount>) -> Result<()> {
+        // this will be hard
     }
 }
 
+#[account]
+pub struct GameAccount {
+    pub game_id: u64,
+    pub is_betting_open: bool,
+    pub total_bettors: u64,
+    pub is_game_over: bool,
+    pub result_runs: Option<u16>,
+    pub result_wickets: Option<u8>,
+    pub prize_distributed: bool,
+    pub bump: u8,
+}
+
+#[account]
+pub struct BettorAccount {
+    pub runs: u16,
+    pub wickets: u8,
+    pub claimed: bool,
+    pub bump: u8,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(game_id: u64)]
+pub struct CreateGame<'info> {
+    #[account(init, payer = signer, space = 8 + 8 + 1 + 8 + 1 + 3 + 2 + 1 + 1, seeds = [b"game", game_id.to_le_bytes().as_ref()], bump)]
+    pub game_account: Account<'info, GameAccount>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct PlaceBet<'info> {
+    #[account(init, payer = signer, space = 8 + 2 + 1 + 1 + 1, seeds = [b"bettor-account", game_account.key().as_ref(), signer.key().as_ref()], bump)]
+    pub bettor_account: Account<'info, BettorAccount>,
+    #[account(mut, seeds= [b"game", game_account.game_id.to_le_bytes().as_ref()], bump = game_account.bump)]
+    pub game_account: Account<'info, GameAccount>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateGameAccount<'info> {
+    #[account(mut)]
+    pub game_account: Account<'info, GameAccount>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
--- tests/cricket-betting.ts
+++ tests/cricket-betting.ts
@@ -1,16 +1,57 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { CricketBetting } from "../target/types/cricket_betting";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
 
 describe("cricket-betting", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.cricketBetting as Program<CricketBetting>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const gameId = new anchor.BN(1);
+  let gameAccountPDA: PublicKey
+
+  it("create a game", async () => {
+
+    [gameAccountPDA] = PublicKey.findProgramAddressSync(
+      [Buffer.from("game"), gameId.toArrayLike(Buffer, "le", 8)],
+      program.programId
+    )
+
+    // Create game
+    const tx = await program.methods.createGame(gameId).accounts({
+      gameAccount: gameAccountPDA,
+      signer: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId
+    }).rpc()
+
+    console.log("Game created in tx:", tx);
+
+    const account = await program.account.gameAccount.fetch(gameAccountPDA);
+    console.log("Game account:", account);
+
+    //console.log("Your transaction signature", tx);
+  });
+
+  it("Place a bet", async () => {
+
+    const [bettorAccountPDA] = PublicKey.findProgramAddressSync(
+      [Buffer.from("bettor-account"), gameAccountPDA.toBuffer(), provider.wallet.publicKey.toBuffer()],
+      program.programId
+    )
+
+    const tx = await program.methods.placeBet(150, 4).accounts({
+      bettorAccount: bettorAccountPDA,
+      gameAccount: gameAccountPDA,
+      signer: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId
+    }).rpc();
+
+    console.log("Bet placed in tx:", tx);
+
+    const bet = await program.account.bettorAccount.fetch(bettorAccountPDA);
+    console.log("Bettor account:", bet);
   });
 });
